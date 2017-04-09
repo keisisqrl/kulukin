0e5f36a7f2ae8e46033789443d9c368b57317812
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-04-09 11:43:42 -0700

    Update deps for canary

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Kulukin.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:coherence, "~> 0.3"}
+     {:coherence, "~> 0.3"},
+     {:canary, "~> 1.1.0"}
    ]
   end
 
