0fed0d9f2124a549a3e21fc04e60fcd24d033205
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-04-09 12:55:24 -0700

    Doc

--- web/models/abilities.ex
+++ web/models/abilities.ex
@@ -1,6 +1,7 @@
 alias Kulukin.User
 
 defimpl Canada.Can, for: User do
+  # User abilities for User
   def can?(%User{admin: true},_,_), do: true
   def can?(%User{id: user_id}, action, %User{id: user_id}) when action in
     [:show, :edit, :update], do: true
