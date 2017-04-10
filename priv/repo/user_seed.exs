5f5abd0f18fe2d04b10967c9296c3867c7c4dbe7
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-04-09 19:19:56 -0700

    Added seed user in priv/repo/user_seed.exs

---
+++ priv/repo/user_seed.exs
@@ -0,0 +1,12 @@
+Kulukin.Repo.delete_all Kulukin.User
+
+Kulukin.User.changeset(%Kulukin.User{},
+  %{
+    name: "Default Admin",
+    email: "admin@example.com",
+    password: "opensesame",
+    password_confirmation: "opensesame",
+    admin: true
+  })
+|> Kulukin.Repo.Insert!
+|> Coherence.ControllerHelpers.confirm!
