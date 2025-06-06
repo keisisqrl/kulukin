ebe7f52399b29e1b1943dcbbcfee5881c62f6aef
Amy W <49384+keisisqrl@users.noreply.github.com>
2025-06-06 18:20:34 +0000

    initial schema for dev

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kulukin
 Kulukin: the distributed library
+
+This is probably the fourth attempt at this. I'm trying sqlpage this time.
+
+Initial schema is **NOT STABLE** until this notice is removed
---
+++ sqlpage/migrations/0001_initial_schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  username TEXT NOT NULL,
+  password_hash TEXT NOT NULL,
+  email TEXT NOT NULL,
+  admin INTEGER NOT NULL DEFAULT 0,
+);
+
+CREATE TABLE login_sessions (
+  session_id TEXT PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  start_time INTEGER NOT NULL DEFAULT (unixepoch('now')),
+)
+
+CREATE TABLE item (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  owner_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  shelf_id INTEGER REFERENCES shelf(id) ON DELETE SET NULL
+);
+
+CREATE TABLE shelf (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  owner_id INTEGER REFERENCES user(id) ON DELETE CASCADE
+);
