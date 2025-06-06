e3cfeb1dc15581e949b28f0a4520808bf1ba04bc
Amy W <49384+keisisqrl@users.noreply.github.com>
2025-06-06 11:41:39 -0700

    basic frontpage

---
+++ index.sql
@@ -0,0 +1,27 @@
+SELECT 'shell' as component,
+  'Kulukin' as title,
+  'A community-powered library for sharing items with friends, family, and neighbors.' as description,
+  'dark' as theme,
+  'Login' as menu_item
+;
+
+
+SELECT 'hero' as component,
+  'Kulukin' as title,
+  'The distributed library' as description,
+  '/images/library-splash.jpg' as image;
+SELECT
+  'Any item' as title,
+  'Books, tools, *anything* you want to loan' as description_md,
+  'infinity' as icon,
+  'blue' as color;
+SELECT
+  'Anywhere' as title,
+  'Create shelves for wherever your items live' as description,
+  'building-community' as icon,
+  'yellow' as color;
+SELECT
+  'Community-powered' as title,
+  'Share your items with friends, family, and neighbors' as description,
+  'heart-handshake' as icon,
+  'red' as color;
