17b9b3c8dd59ffad26d6566c7aa5a44fced0ac6e
Amy W <49384+keisisqrl@users.noreply.github.com>
2025-06-06 17:14:00 -0700

    actual final initial schema (?)

--- sqlpage/migrations/0001_initial_schema.sql
+++ sqlpage/migrations/0001_initial_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user (
   id INTEGER PRIMARY KEY,
-  username TEXT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
   email TEXT NOT NULL,
   admin INTEGER NOT NULL DEFAULT 0
